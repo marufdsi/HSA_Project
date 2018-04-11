e3fc500c612e249ff2b79dd111a3be41430768ca
Md Maruf Hossain <mhossa10@cph-i1.uncc.edu>
2018-04-10 20:10:07 -0400

    FIrst commit

---
+++ opencl/cfd/cfd_job.sh
@@ -0,0 +1,2 @@
+cd Projects/HSA/rodinia_3.1/opencl/cfd
+./euler3d ../../data/cfd/fvcorr.domn.097K -t gpu -d 0
---
+++ opencl/kmeans/kmean_job.sh
@@ -0,0 +1,2 @@
+cd Projects/HSA/rodinia_3.1/opencl/kmeans
+./kmeans -o -i ../../data/kmeans/kdd_cup 
