7c385f67c518e6628a713e8becec1331a04b736a
Maruf <maruf.csea1@gmail.com>
2018-05-07 02:02:53 -0400

    time updated

--- openmp/kmeans/kmeans_openmp/kmeans.c
+++ openmp/kmeans/kmeans_openmp/kmeans.c
@@ -233,7 +233,7 @@ int main(int argc, char **argv) {
         printf("\n\n");
     }
 */
-	printf("Time for process: %f\n", timing);
+	printf("Time for process: %f\n", timing*1000);
 
     free(attributes);
     free(cluster_centres[0]);
